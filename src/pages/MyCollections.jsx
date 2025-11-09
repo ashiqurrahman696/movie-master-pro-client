1dd8e5705d639de45c1d687df908bcf9bf1a6d26
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-11-09 16:41:09 +0600

    delete movie when delete button clicked in in my collections page

--- src/pages/MyCollections.jsx
+++ src/pages/MyCollections.jsx
@@ -1,20 +1,55 @@
 import { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
 import { Link } from "react-router";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
 
 const MyCollections = () => {
     const {user} = useAuth();
-    const [collections, setCollections] = useState([]);
+    const [movies, setMovies] = useState([]);
     useEffect(() => {
         if(user?.email){
             fetch(`http://localhost:3000/my-collection?email=${user.email}`)
                 .then(res => res.json())
                 .then(data => {
-                    setCollections(data);
+                    setMovies(data);
                     console.log(data);
                 })
         }
     }, [user]);
+
+    const handleDeleteMovie = id => {
+        Swal.fire({
+            title: "Are you sure to delete?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "dodgerblue",
+            cancelButtonColor: "crimson",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/movies/${id}`, {
+                    method: "DELETE",
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if(data.deletedCount){
+                            Swal.fire({
+                                title: "Movie removed!",
+                                text: "Your movie has been removed.",
+                                icon: "success"
+                            });
+                            const remainingMovies = movies.filter(movie => movie._id !== id);
+                            setMovies(remainingMovies);
+                        }
+                    })
+                    .catch(error => {
+                        toast.error(error);
+                    })
+            }
+        });
+    }
     return (
         <div className="space-y-5 max-w-7xl mx-auto px-4 py-6">
             <h1 className="text-center font-semibold text-4xl">My Collection</h1>
@@ -31,17 +66,17 @@ const MyCollections = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {collections.map((item, index) => <tr key={item._id}>
+                        {movies.map((movie, index) => <tr key={movie._id}>
                             <th>{index + 1}</th>
-                            <td>{item.title}</td>
+                            <td>{movie.title}</td>
                             <td>
-                                <img src={item.posterUrl} className="w-12" alt={item.title} />
+                                <img src={movie.posterUrl} className="w-12" alt={movie.title} />
                             </td>
-                            <td>{item.genre}</td>
+                            <td>{movie.genre}</td>
                             <td>
                                 <div className="flex gap-2">
-                                    <Link to={`/movies/update/${item._id}`} className="btn btn-info">Edit</Link>
-                                    <button className="btn btn-error">Delete</button>
+                                    <Link to={`/movies/update/${movie._id}`} className="btn btn-info">Edit</Link>
+                                    <button onClick={() => handleDeleteMovie(movie._id)} className="btn btn-error">Delete</button>
                                 </div>
                             </td>
                         </tr>)}
