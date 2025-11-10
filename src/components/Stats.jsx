7c6718acc77e33f2f4a2dc95a2ca6549d032ef68
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-11-10 19:03:59 +0600

    updated ui of stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import AOS from "aos";
+import { BiCameraMovie } from "react-icons/bi";
+import { FaUsers } from "react-icons/fa";
 
 const Stats = ({movies}) => {
     const [users, setUsers] = useState([]);
@@ -11,14 +13,24 @@ const Stats = ({movies}) => {
     }, [])
     return (
         <div className="max-w-7xl mx-auto px-4 py-6 flex justify-center items-center" data-aos="fade-left">
-            <div className="stats shadow">
-                <div className="stat">
-                    <div className="stat-title">Total Movies</div>
-                    <div className="stat-value">{movies.length}</div>
+            <div className="flex gap-5 flex-wrap">
+                <div className="card bg-base-300 w-80 shadow-sm">
+                    <div className="card-body flex-row items-center gap-4">
+                        <BiCameraMovie size={56} />
+                        <div>
+                            <h2 className="card-title">Total movies</h2>
+                            <p className="text-4xl font-bold">{movies.length}</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="stat">
-                    <div className="stat-title">Total Users</div>
-                    <div className="stat-value">{users.length}</div>
+                <div className="card bg-base-300 w-80 shadow-sm">
+                    <div className="card-body flex-row items-center gap-4">
+                        <FaUsers size={56} />
+                        <div>
+                            <h2 className="card-title">Total users</h2>
+                            <p className="text-4xl font-bold">{users.length}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
