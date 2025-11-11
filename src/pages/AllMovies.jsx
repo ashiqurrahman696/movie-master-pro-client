191a0a763d13f1e4da2a1a2f4b17c883dc0725da
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-11-11 07:02:14 +0600

    fixed responsive issue in all movies page

--- src/pages/AllMovies.jsx
+++ src/pages/AllMovies.jsx
@@ -55,7 +55,7 @@ const AllMovies = () => {
     return (
         <div className="space-y-5 max-w-7xl mx-auto px-4 py-6">
             <h1 className="text-center font-semibold text-4xl">All Movies</h1>
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 items-start">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 items-start">
                 <div>
                     <h2 className="font-semibold text-xl mb-4">Filter by Rating</h2>
                     <select className="select w-full" onChange={(e) => setOption(e.target.value)}>
@@ -66,11 +66,11 @@ const AllMovies = () => {
                     <input type="number" name="rating" onInput={(e) => setRatingValue(Number(e.target.value))} className="input w-full mt-2 mb-4" />
                     <button onClick={applyRating} className="btn btn-primary">Apply Rating</button>
                     <h2 className="font-semibold text-xl my-4">Filter by Genres</h2>
-                    <ul className="bg-base-300 rounded-box w-full p-4 flex sm:flex-col flex-wrap justify-center gap-4">
+                    <ul className="bg-base-300 rounded-box w-full p-4 flex md:flex-col flex-wrap justify-center gap-4">
                         {genres.map(genre => <li onClick={(e) => filterByGenre(e.target, genre.name)} key={genre.id} className={`duration-300 p-2 rounded cursor-pointer flex items-center gap-4`}>{genre.name}</li>)}
                     </ul>
                 </div>
-                <div className="grid lg:col-span-3 md:col-span-2 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                <div className="grid lg:col-span-2 xl:col-span-3 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
                     {movies.map(movie => <MovieCard key={movie._id} movie={movie} />)}
                 </div>
             </div>
